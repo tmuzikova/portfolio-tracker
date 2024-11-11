563ebc1aee82a16110fb1b696733addd7a4d622e
tmuzikova <tereza.muzikova@gmail.com>
2024-11-11 15:14:54 +0100

    feat: add homepage link to navbar logo

--- src/app/Layout/Navbar.tsx
+++ src/app/Layout/Navbar.tsx
@@ -1,10 +1,11 @@
 import { NavigationMenu } from '@/components/ui/navigation-menu';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <div className="bg-[hsl(var(--sidebar-accent))] py-5">
       <NavigationMenu className="px-4 text-[32px] font-bold text-[hsl(var(--sidebar-primary))]">
-        portfolio tracker
+        <Link to="/">portfolio tracker</Link>
       </NavigationMenu>
     </div>
   );
