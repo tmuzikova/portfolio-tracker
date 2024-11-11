4039fd435bbe98bc43d8835d596b5a8afb7df3bb
tmuzikova <tereza.muzikova@gmail.com>
2024-11-11 16:29:24 +0100

    feat: add footer

--- src/app/Layout/Layout.tsx
+++ src/app/Layout/Layout.tsx
@@ -2,15 +2,19 @@ import { Outlet } from 'react-router-dom';
 import { AppSidebar } from './AppSidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { Navbar } from './Navbar';
+import { Footer } from './Footer';
 
 export const Layout = () => {
   return (
     <>
       <SidebarProvider>
         <AppSidebar />
-        <main className="w-full">
-          <Navbar />
-          <Outlet />
+        <main className="flex w-full flex-col">
+          <div className="flex-grow">
+            <Navbar />
+            <Outlet />
+          </div>
+          <Footer />
         </main>
       </SidebarProvider>
     </>
