a0f0e4932055dcbd59f23c39ae1df45fe8797449
tmuzikova <tereza.muzikova@gmail.com>
2024-11-11 11:56:30 +0100

    chore: update mock data for pie chart

--- src/features/dashboard/components/DiversificationCharts.tsx
+++ src/features/dashboard/components/DiversificationCharts.tsx
@@ -3,45 +3,33 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   ChartConfig,
   ChartContainer,
-  ChartLegend,
-  ChartLegendContent,
   ChartTooltip,
   ChartTooltipContent,
 } from '@/components/ui/chart';
 
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
+
+//to be replaced with real data
 const chartData = [
-  { browser: 'chrome', visitors: 275, fill: 'var(--color-chrome)' },
-  { browser: 'safari', visitors: 200, fill: 'var(--color-safari)' },
-  { browser: 'firefox', visitors: 187, fill: 'var(--color-firefox)' },
-  { browser: 'edge', visitors: 173, fill: 'var(--color-edge)' },
-  { browser: 'other', visitors: 90, fill: 'var(--color-other)' },
+  { stock: 'AAPL', portfolioShare: 10, fill: '#007aff' },
+  { stock: 'MSFT', portfolioShare: 12, fill: '#00a1f1' },
+  { stock: 'GOOGL', portfolioShare: 15, fill: '#ea4335' },
+  { stock: 'AMZN', portfolioShare: 8, fill: '#ff9900' },
+  { stock: 'TSLA', portfolioShare: 20, fill: '#cc0000' },
+  { stock: 'NVDA', portfolioShare: 10, fill: '#76b900' },
+  { stock: 'KO', portfolioShare: 5, fill: '#f40009' },
+  { stock: 'JNJ', portfolioShare: 12, fill: '#6a737b' },
 ];
 const chartConfig = {
-  visitors: {
-    label: 'Visitors',
-  },
-  chrome: {
-    label: 'Chrome',
-    color: 'hsl(var(--chart-1))',
-  },
-  safari: {
-    label: 'Safari',
-    color: 'hsl(var(--chart-2))',
-  },
-  firefox: {
-    label: 'Firefox',
-    color: 'hsl(var(--chart-3))',
-  },
-  edge: {
-    label: 'Edge',
-    color: 'hsl(var(--chart-4))',
-  },
-  other: {
-    label: 'Other',
-    color: 'hsl(var(--chart-5))',
-  },
+  AAPL: { label: 'Apple Inc. (AAPL)', color: '#007aff' },
+  MSFT: { label: 'Microsoft Corporation (MSFT)', color: '#00a1f1' },
+  GOOGL: { label: 'Alphabet Inc. (GOOGL)', color: '#ea4335' },
+  AMZN: { label: 'Amazon.com, Inc. (AMZN)', color: '#ff9900' },
+  TSLA: { label: 'Tesla, Inc. (TSLA)', color: '#cc0000' },
+  NVDA: { label: 'NVIDIA Corporation (NVDA)', color: '#76b900' },
+  KO: { label: 'The Coca-Cola Company (KO)', color: '#f40009' },
+  JNJ: { label: 'Johnson & Johnson (JNJ)', color: '#6a737b' },
 } satisfies ChartConfig;
 
 const diversificationTypes = ['Aktiva', 'Typ aktiva', 'Sektor', 'Měna', 'Země'];
@@ -82,7 +70,12 @@ export const DiversificationCharts = () => {
         >
           <PieChart>
             <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-            <Pie data={chartData} dataKey="visitors" label nameKey="browser" />
+            <Pie
+              data={chartData}
+              dataKey="portfolioShare"
+              label
+              nameKey="stock"
+            />
           </PieChart>
         </ChartContainer>
       </CardContent>
