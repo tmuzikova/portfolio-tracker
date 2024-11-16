08ed91a239c93dd75fa60aecb856978ef8218a14
tmuzikova <tereza.muzikova@gmail.com>
2024-11-16 23:16:44 +0100

    feat: save symbol list to indexedDB

--- src/stores/fetchSymbolList.ts
+++ src/stores/fetchSymbolList.ts
@@ -1,3 +1,5 @@
+import { openDB } from 'idb';
+
 export type SymbolList = {
   symbol: string;
   exchange: string;
@@ -7,6 +9,19 @@ export type SymbolList = {
 };
 
 export const fetchSymbolList = async (): Promise<SymbolList[]> => {
+  const db = await openDB('symbolDB', 1, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains('symbols')) {
+        db.createObjectStore('symbols', { keyPath: 'symbol' });
+      }
+    },
+  });
+
+  const databaseData = await db.getAll('symbols');
+  if (databaseData.length > 0) {
+    return databaseData;
+  }
+
   const api_FMP_key = import.meta.env.VITE_FMP_API_KEY;
   const response = await fetch(
     `https://financialmodelingprep.com/api/v3/stock/list?apikey=${api_FMP_key}`,
@@ -15,6 +30,13 @@ export const fetchSymbolList = async (): Promise<SymbolList[]> => {
   if (!response.ok) {
     throw new Error(`Failed to fetch Symbol List: ${response.statusText}`);
   }
+  const data = await response.json();
 
-  return response.json();
+  const tx = db.transaction('symbols', 'readwrite');
+  for (const symbol of data) {
+    await tx.store.put(symbol);
+  }
+  await tx.done;
+
+  return data;
 };
