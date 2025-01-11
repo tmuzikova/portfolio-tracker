72c9eb5e8ef554efc35c196c319d95a3ea82ad94
tmuzikova <tereza.muzikova@gmail.com>
2025-01-11 16:05:31 +0100

    feat: update fallback logo SVG

--- src/components/FallbackLogo.tsx
+++
@@ -1,6 +0,0 @@
-import { FC, SVGProps } from 'react';
-import FallbackLogoSvg from '@/assets/fallback_logo.svg';
-
-export const FallbackLogo: FC<SVGProps<SVGSVGElement>> = (props) => (
-  <FallbackLogoSvg {...props} />
-);
