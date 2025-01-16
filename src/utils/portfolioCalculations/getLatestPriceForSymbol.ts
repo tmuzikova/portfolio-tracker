55a2aac5ef20646a6807c8983fd0c3e21a7bb261
tmuzikova <tereza.muzikova@gmail.com>
2025-01-16 16:56:19 +0100

    feat: add custom hook for single stock historical prices

--- src/utils/portfolioCalculations/getLatestPriceForSymbol.ts
+++ src/utils/portfolioCalculations/getLatestPriceForSymbol.ts
@@ -2,8 +2,11 @@ import { HistoricalPriceData } from '@/types/historicalPrices';
 
 export const getLatestPriceForSymbol = (
   symbol: string,
-  priceData: HistoricalPriceData[] = [],
-) => {
-  const priceInfo = priceData?.find((price) => price.symbol === symbol);
+  priceData: HistoricalPriceData | HistoricalPriceData[] = [],
+): number => {
+  const priceArray = Array.isArray(priceData) ? priceData : [priceData];
+
+  const priceInfo = priceArray.find((price) => price.symbol === symbol);
+
   return priceInfo?.historical?.[0]?.close || 0;
 };
