d61e1f44b66a15b7351ece7cce61470132792907
tmuzikova <tereza.muzikova@gmail.com>
2025-01-25 19:51:08 +0100

    feat: add ProtectedRoute component

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '@/app/AuthContextProvider';
+import { LoadingState } from './LoadingState';
+
+type ProtectedRouteProps = {
+  children: React.ReactNode;
+};
+
+export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { session, loading } = useAuth();
+
+  if (loading) {
+    return <LoadingState />;
+  }
+
+  if (!session) {
+    return <Navigate to="/signin" />;
+  }
+
+  return children;
+};
