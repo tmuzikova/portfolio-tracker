b47386a277b3cf0ccead48853dbae8cd6bef0968
tmuzikova <tereza.muzikova@gmail.com>
2025-01-25 15:33:04 +0100

    style: update app logo, header

--- src/app/Layout/Header.tsx
+++ src/app/Layout/Header.tsx
@@ -1,14 +1,17 @@
 import { SidebarTrigger } from '@/components/ui/sidebar';
-import { MenuIcon } from 'lucide-react';
+import { ChartNoAxesCombined, MenuIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <div className="bg-[hsl(var(--sidebar-accent))] py-5">
-      <header className="flex flex-row justify-between px-4 text-[32px] font-bold text-[hsl(var(--sidebar-primary))]">
-        <Link to="/" className="whitespace-nowrap">
-          portfolio tracker
-        </Link>
+      <header className="flex flex-row justify-between px-4 text-[38px] font-bold tracking-wider text-[hsl(var(--sidebar-primary))]">
+        <div className="flex flex-row items-center gap-2">
+          <ChartNoAxesCombined className="h-[38px] w-[38px]" />
+          <Link to="/" className="whitespace-nowrap">
+            Trackfolio
+          </Link>
+        </div>
         <SidebarTrigger
           customIcon={<MenuIcon />}
           className="py-6 pr-3 hover:bg-transparent md:hidden"
