67e002e3757775b35c5cc1aee0b8a7018da97098
tmuzikova <tereza.muzikova@gmail.com>
2025-01-29 13:02:04 +0100

    fix: incorporate header logo to link

--- src/app/Layout/Header.tsx
+++ src/app/Layout/Header.tsx
@@ -6,12 +6,14 @@ export const Header = () => {
   return (
     <div className="bg-[hsl(var(--sidebar-accent))] py-5">
       <header className="flex flex-row justify-between px-4 text-[38px] font-bold tracking-wider text-[hsl(var(--sidebar-primary))]">
-        <div className="flex flex-row items-center gap-2">
+        <Link
+          to="/"
+          className="flex flex-row items-center gap-2 whitespace-nowrap"
+        >
           <ChartNoAxesCombined className="h-[38px] w-[38px]" />
-          <Link to="/" className="whitespace-nowrap">
-            Trackfolio
-          </Link>
-        </div>
+          Trackfolio
+        </Link>
+
         <SidebarTrigger
           customIcon={<MenuIcon />}
           className="py-6 pr-3 hover:bg-transparent md:hidden"
